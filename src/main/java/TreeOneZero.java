5a1678c39099a31c54e9937f0bff0c55a6ac429c
hyunjialee <hyjialee@gmail.com>
2023-12-14 11:59:27 -0500

    add, contain, and delete methods

--- src/main/java/TreeOneZero.java
+++ src/main/java/TreeOneZero.java
@@ -4,13 +4,64 @@
 public class TreeOneZero {
     Node root;
 
-    private Node addRecursive(Node current, Integer value){
-        return null;
+    private Node addRecursive(Node current, Integer value) {
+        if (current == null) {
+            return new Node(value);
+        }
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        }
+        return current;
     }
 
-    public void add (Integer value){
-
+    public void add(Integer value) {
+        this.root = addRecursive(root, value);
     }
 
+    private boolean containsNodeRecursive(Node current, Integer value) {
+        if (current == null) {
+            return false;
+        }
+        if (value.equals(current.value)) {
+            return true;
+        }
+        if (value < current.value) {
+            return containsNodeRecursive(current.left, value);
+        } else {
+            return containsNodeRecursive(current.right, value);
+        }
+    }
+
+    public boolean containsNode(Integer value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    private Node deleteRecursive(Node current, Integer value) {
+        if (current == null) {
+            return null;
+        }
+        if (value.equals(current.value)) {
+                // Case 1: Node to be deleted has no children (leaf node)
+                if (current.left == null && current.right == null) {
+                    return null;
+                }
+                // Case 2: Node to be deleted has one child
+                if (current.right == null) {
+                    return current.left;
+                } else if (current.left == null) {
+                    return current.right;
+                }
+        } return current;
+    }
+
+    public void deleteNode (Integer value){
+            root = deleteRecursive(root, value);
+    }
+
+
+    //Traversing the Tree
+    // - Depth First search, Breadth-First search,
 
 }
--- src/test/java/TreeTest.java
+++ src/test/java/TreeTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TreeTest {
@@ -5,10 +6,15 @@ public class TreeTest {
     public void givenABinaryTree_WhenAddingElements_ThenTreeContainsThoseElements() {
         TreeOneZero bt = new TreeOneZero();
 
-        assertTrue(bt.containsNode(6));
-        assertTrue(bt.containsNode(4));
+        bt.add(6);
+        bt.add(4);
+        bt.add(1);
 
-        assertFalse(bt.containsNode(1));
+
+        Assert.assertTrue(bt.containsNode(6));
+        Assert.assertTrue(bt.containsNode(4));
+
+        Assert.assertFalse(bt.containsNode(1));
     }
 
 }
